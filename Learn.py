04e17e82d4c3e708a53b7281fe4582b22fe3771c
lolalola <lola.emin@gmail.com>
2019-02-25 14:01:52 -0600

    Python notes

---
+++ Learn.py
@@ -0,0 +1,5 @@
+monthly_pay = 5000.0
+annual_pay = monthly_pay*12
+print('Your annual pay is $',
+      format(annual_pay, ',.2f'),
+      sep='')
---
+++ columns.py
@@ -0,0 +1,18 @@
+# This program displays the following
+# floating-point numbers in a column
+# with their decimal points aligned
+num1=127.899
+num2=3465.148
+num3=3.667
+num4=332.421
+num5=88.442
+num6=999.555
+
+
+print(format(num1, '7.2f'))
+print(format(num2, '7.2f'))
+print(format(num3, '7.2f'))
+print(format(num4, '7.2f'))
+print(format(num5, '7.2f'))
+print(format(num6, '7.2f'))
+
